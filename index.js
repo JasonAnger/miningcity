0e50de2b476819efa73415d0eb75d339d64f5958
Jay An <jayandn1999@gmail.com>
2021-01-04 03:27:40 +0700

    Lay va hien thi bai viet.

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
+const fetch = require('node-fetch')
 const path = require('path')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 const app = express()
 
-app.set('views', path.join(__dirname, 'views'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.set("view engine", "ejs")
+app.set("views", "./views")
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -19,7 +18,12 @@ app.use(express.static('public'))
 
 
 app.get('/', function(req,res){
-    res.sendFile('./public/index.html')
+    res.render('index')
+})
+
+app.get('/btcv', async (req,res) => {
+    const data = await fetch('http://localhost:3000/api/5ff218fb5b4c0d0910ff4c66').then(res => res.json())
+    res.render('btcv', {post: data})
 })
 
 app.use('/*', function(req,res){
