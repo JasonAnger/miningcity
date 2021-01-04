b34ddcacc457494149993e133616fbf9b17df9dd
Jay An <jayandn1999@gmail.com>
2021-01-04 18:26:33 +0700

    For Deploy

--- index.js
+++ index.js
@@ -27,13 +27,27 @@ app.use(express.static('public'))
 app.get('/', function(req,res){
     res.render('index')
 })
-
+app.get('/miningcity', async (req,res) => {
+    const post = await Post.findOne({_id: "5ff2e823fc639b57b8c3552b"})
+    res.render('miningcity', {post: post})
+})
 app.get('/btcv', async (req,res) => {
     //const data = await fetch('http://localhost:3000/api/5ff218fb5b4c0d0910ff4c66').then(res => res.json())
     const post = await Post.findOne({_id: "5ff218fb5b4c0d0910ff4c66"})
     res.render('btcv', {post: post})
 })
-
+app.get('/elcash', async (req,res) => {
+    const post = await Post.findOne({_id: "5ff218fb5b4c0d0910ff4c66"})
+    res.render('elcash', {post: post})
+})
+app.get('/muagoidao', async (req,res) => {
+    const post = await Post.findOne({_id: "5ff2e216fc639b57b8c35529"})
+    res.render('muagoidao', {post: post})
+})
+app.get('/sangiaodich', async (req,res) => {
+    const post = await Post.findOne({_id: "5ff2df05fc639b57b8c35527"})
+    res.render('sangiaodich', {post: post})
+})
 app.use('/*', function(req,res){
     res.redirect('/')
 })
